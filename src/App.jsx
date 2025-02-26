24c0c1ea07c2102a183b50d27957c5ca561e94f9
gigleson <kaushalranjit11@gmail.com>
2025-02-26 19:21:34 +0545

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ProfilePage from "./pages/ProfilePage";
+import Navbar from "./components/Navbar";
 
-
-
-function App() {
-
-
+const App = () => {
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
-  )
-}
+    <AuthProvider>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/profile/:id" element={<ProfilePage />} />
+      </Routes>
+    </AuthProvider>
+  );
+};
 
-export default App
+export default App;
