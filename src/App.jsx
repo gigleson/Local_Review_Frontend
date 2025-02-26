25ea084b8915ba54fa07679bdf20296061a2f353
gigleson <kaushalranjit11@gmail.com>
2025-02-26 23:47:43 +0545

    changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,50 @@
 import { Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
+import { ChatProvider } from "./contexts/ChatContext";
 import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import MessagesPage from "./pages/MessagesPage";
+import Navbar from "./components/Navbar";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-import ProfilePage from "./pages/ProfilePage";
-import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
+import CreatePostPage from "./pages/CreatePostPage"; 
+import EditProfilePage from "./pages/EditProfilePage"; 
 
 const App = () => {
   return (
     <AuthProvider>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/profile/:id" element={<ProfilePage />} />
-      </Routes>
+      <ChatProvider>
+        <Navbar />
+        <div className="pt-20">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/create-post" element={<CreatePostPage />} />
+          <Route path="/edit-profile" element={<EditProfilePage />} />
+          
+
+          {/* Protected Routes */}
+          <Route
+            path="/profile/:id"
+            element={
+              <ProtectedRoute>
+                <ProfilePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/messages"
+            element={
+              <ProtectedRoute>
+                <MessagesPage />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+        </div>
+      </ChatProvider>
     </AuthProvider>
   );
 };
